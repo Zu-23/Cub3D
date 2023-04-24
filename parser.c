89fcad4e0084f989d87c598f9eb1bf822260b45d
zu-23 <h.zakaria52@gmail.com>
2023-04-23 23:07:34 -0400

    only map parsin left

--- parser.c
+++ parser.c
@@ -6,7 +6,6 @@
 #include<string.h>
 #include<stdlib.h>
 
-
 /*
 DATA:
 	1- DIRECTION TEXTURES: IT HAS 2 ARGUMENTS *ID AND *DESTINATION
@@ -53,26 +52,76 @@ int	ft_isspace(int c)
 
 int	fill_texture(char *line, t_texture texture)
 {
+	char	*after_xpm;
+
+	after_xpm = ft_strstr(line, ".xpm") + 4;
+	while (after_xpm != (char *)4 && *after_xpm != '\n')
+	{
+		if (*after_xpm != ' ')
+			return (-1);
+		after_xpm++;
+	}
 	ft_strcpy(texture.id, ft_strtok(line, " "));
-	ft_strcpy(texture.dest, ft_strtok(NULL, " "));
-	printf("id: %s\n dest: %s\n", texture.id, texture.dest);
+	ft_strcpy(texture.dest, ft_strtok(NULL, " \n"));
+	printf("id: %s\ndest: %s\n", texture.id, texture.dest);
 	if (texture.dest == NULL || ft_strstr(texture.dest, ".xpm") == NULL)
 		return (-1);
 	return (0);
 }
 
+int	check_rgb(char *rgb)
+{
+	int	i;
+
+	i = 0;
+	printf("RGB: %s\n", rgb);
+	while (rgb[i])
+	{
+		//printf("rgb |%c|\n",rgb[i]);
+		if (ft_isdigit(rgb[i]) == 0)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
 int	fill_color(char *line, t_color color)
 {
+	char	*rgb[3];
+	int		i;
+
+	i = 0;	
 	ft_strcpy(color.id, ft_strtok(line, " "));
-	color.red = ft_atoi(ft_strtok(NULL, " ,"));
-	color.green = ft_atoi(ft_strtok(NULL, " ,"));
-	color.blue = ft_atoi(ft_strtok(NULL, " ,"));
+	while (i < 3)
+	{
+		rgb[i] = ft_strtok(NULL, " ,\n");
+		if (check_rgb(rgb[i++]))
+			return (-1);
+	}
+	color.red = ft_atoi(rgb[0]);
+	color.green = ft_atoi(rgb[1]);
+	color.blue = ft_atoi(rgb[2]);
 	if ((color.red < 0 || color.red > 255)
 		|| (color.green < 0 || color.green > 255)
 		|| (color.blue < 0 || color.blue > 255))
 		return (-1); 
 
 	printf("id %s red %d green %d blue %d\n", color.id,color.red, color.green, color.blue);
+	return (0);
+}
+
+int	parse_color(char *line, t_data *data)
+{
+	if (line[0] == 'C' && line[1] == ' ')
+	{
+		if (fill_color(line, data->c))
+			return (-1);
+	}
+	else if (line[0] == 'F' && line[1] == ' ')
+	{
+		if (fill_color(line, data->f))
+			return (-1);
+	}
 	return (0);
 }
 
@@ -98,16 +147,25 @@ int	parse_texture(char *line, t_data *data)
 		if (fill_texture(line, data->we))
 			return (-1);
 	}
-	else if (line[0] == 'C' && line[1] == ' ')
-	{
-		if (fill_color(line, data->c))
-			return (-1);
-	}
-	else if (line[0] == 'F' && line[1] == ' ')
-	{
-		if (fill_color(line, data->f))
-			return (-1);
-	}
+	return (0);
+}
+
+int	parse_map(char *line, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] == ' ')
+}
+
+int	evaluate_parse_functions(char *line, t_data *data)
+{
+	if (parse_texture(line, data))
+		return (-1);
+	if (parse_color(line, data))
+		return (-1);
+	if (parse_map(line, data))
+		return (-1);
 	return (0);
 }
 
@@ -122,7 +180,7 @@ int	parse_file(int fd, t_data *data)
 		ft_error("Error\nin file handling\n");
 	while (line)
 	{
-		if (parse_texture(line, data)) //|| parse_floor_ceiling(line, data))
+		if (evaluate_parse_functions(line, data))
 		{
 			error = -2;
 			break ;
@@ -150,3 +208,10 @@ int main(int ac, char **av)
 	else
 		ft_error("Error\nwrong number of arguments\n");
 }
+
+
+//plan for parsin the map
+//1- we check if the line is valid (valid means it contains only 0 1 N,S,E,W)
+//2- remove any spaces in the line
+//3- save the line in the map element
+//4- 
