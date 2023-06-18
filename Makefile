9369cad402952918cc4e5d37e98289cce4f835b6
Ayoub Lemsafi <alemsafi.student@1337.ma>
2023-06-18 19:38:43 +0100

    fixed parsing error when putting newline with empty spaces

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ FLAGS :=	-Wall -Wextra -Werror
 LIBS2 :=	-lmlx -framework OpenGL -framework AppKit
 LIBFT :=	lib_gnl/lib/libft.a
 GNL	:=		lib_gnl/gnl/get_next_line.o
-SRC :=		parser.c raycaster.c main.c
+SRC :=		parser.c main.c
 OBJ	:=		$(SRC:.c=.o)
 
 all: $(NAME)
--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int	main(int ac, char **av)
 		fd = open(av[1], O_RDONLY);
 		parse_file(fd, &data);
 		check_data(&data);
-		find_player_location(&data);
-		printf("player x %d y %d angle %d\n", data.px, data.py, data.player_angle);
+		//find_player_location(&data);
+		//printf("player x %d y %d angle %d\n", data.px, data.py, data.player_angle);
 	}
 	else
 		ft_error("Error\nwrong number of arguments\n");
